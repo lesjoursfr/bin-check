4e7d783446827f87587eb807b80b76aac7e0c9d6
XhmikosR <xhmikosr@gmail.com>
2023-06-02 07:42:37 +0300

    Replace @xhmikosr/executable with isexe

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import execa from 'execa';
-import executable from '@xhmikosr/executable';
+import isexe from 'isexe';
 
 const binCheck = (bin, args) => {
 	if (!Array.isArray(args)) {
 		args = ['--help'];
 	}
 
-	return executable(bin)
+	return isexe(bin)
 		.then(works => {
 			if (!works) {
 				throw new Error(`Couldn't execute the "${bin}" binary. Make sure it has the right permissions.`);
@@ -22,7 +22,7 @@ binCheck.sync = (bin, args) => {
 		args = ['--help'];
 	}
 
-	if (!executable.sync(bin)) {
+	if (!isexe.sync(bin)) {
 		throw new Error(`Couldn't execute the "${bin}" binary. Make sure it has the right permissions.`);
 	}
 
